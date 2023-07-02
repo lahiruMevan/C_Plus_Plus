ce106fee4b8fc8a29233ec5222ff04e730e6d76a
Mevan <Mevan@LMS>
2023-07-02 07:10:09 +0530

    Add project files.

---
+++ OpenGL/primerPlusC++/getinfo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+//int main()
+//{
+//	int carrots;
+//
+//	std::cout << "How many carrots do you have?" << std::endl;
+//	std::cin >> carrots;
+//
+//	std::cout << "Here are two more. ";
+//	carrots = carrots + 2;
+//
+//	std::cout << "How you have " << carrots << " carrots." << std::endl;
+//
+//
+//	return 0;
+//}
