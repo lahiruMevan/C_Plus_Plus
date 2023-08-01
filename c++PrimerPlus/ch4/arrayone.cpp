e5e261773abfe338e7ac3a1c4b99df9c529da754
Mevan <Mevan@LMS>
2023-08-01 08:01:58 +0530

    ok

---
+++ c++PrimerPlus/ch4/arrayone.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//
+//int main()
+//{
+//	int yams[3];
+//	yams[0] = 7;
+//	yams[1] = 8;
+//	yams[2] = 6;
+//
+//	int yamcosts[3] = { 20, 30, 5 };
+//
+//	std::cout << "Total yams = ";
+//	std::cout << yams[0] + yams[1] + yams[2] << std::endl;
+//	std::cout << "The packages with " << yams[1] << "yams costs";
+//	std::cout << yamcosts[1] << "cents per yam.\n";
+//	int total = yams[0] * yamcosts[0] + yams[1] * yamcosts[1];
+//	total = total + yams[2] * yamcosts[2];
+//	std::cout << "The total yam expense is " << total << " cents.\n";
+//
+//	std::cout << "\nSize of yams array = " << sizeof yams;
+//	std::cout << "bytes.\n";
+//	std::cout << "Size of one element = " << sizeof yams[0];
+//	std::cout << " bytes.\n";
+//
+//
+//
+//	return 0;
+//}
---
+++ c++PrimerPlus/ch4/numstr.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//
+//int main()
+//{
+//	std::cout << "What year was your house built?\n";
+//	int year;
+//	std::cin >> year;
+//	std::cin.get();
+//	std::cout << "What is its street address?\n";
+//	char address[80];
+//	std::cin.getline(address, 80);
+//	std::cout << "Year built: " << year << std::endl;
+//	std::cout << "Address: " << address << std::endl;
+//	std::cout << "Done\n";
+//
+//	return 0;
+//}
