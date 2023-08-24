523a894ec0b526d1960830c67c534ac005d1c668
Mevan <Mevan@LMS>
2023-08-24 15:47:29 +0530

    done

--- c++PrimerPlus/phoneBook/phoneBook.cpp
+++ c++PrimerPlus/phoneBook/phoneBook.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+struct page {
+
+	std::string name;
+	std::string number;
+
+};
+
 
 
 int main()
 {
-	std::cout << "phone book" << std::endl;
+	std::vector<page> book(4);
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		book[i].name = "test" + i;
+		book[i].number = std::string (i + " " + i +" " + i + " " + "-" + (i + 1) + "" + (i + 1) + "" + (i + 1) + "" + (i + 1));
+	}
 
 
 	return 0;
